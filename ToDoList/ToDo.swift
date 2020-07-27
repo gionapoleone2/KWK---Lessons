b45ca21552ffdbdeeea9305da8b3fc3945fcd802
gionapoleone2 <44882125+gionapoleone2@users.noreply.github.com>
2020-07-27 14:00:54 -0400

    Completed Iteration 0 - ToDoList Lesson

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Giovanna Napoleone on 7/27/20.
+//  Copyright © 2020 Giovanna Napoleone. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Giovanna Napoleone on 7/27/20.
+//  Copyright © 2020 Giovanna Napoleone. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var listOfToDo: [ToDoClass] = []
+
+    func createToDo() -> [ToDoClass]{
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass()
+        dogToDo.description = "Walk the Dog"
+        //important is set to false as default
+        
+        return [swiftToDo, dogToDo]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     listOfToDo = createToDo()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+    
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let eachToDo = listOfToDo[indexPath.row]
+        
+        cell.textLabel?.text = eachToDo.description
+        
+        if eachToDo.important{
+            cell.textLabel?.text = "!" + eachToDo.description
+        }
+        
+        else {
+            
+            cell.textLabel?.text = eachToDo.description
+        }
+     
+        
+        return cell
+    }
+    
+
+   
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+ }*/
+}
